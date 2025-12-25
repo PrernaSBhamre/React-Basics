a259e0a29db831ce3d45e55360222109c40b9e99
PrernaBhamre <prernabhamre2324_it@sanjivanicoe.org.in>
2025-12-25 16:01:55 +0530

    Tailwind-props added

--- basic-react-01/src/demo1.js
+++
@@ -1,6 +0,0 @@
-function Demo1(){
-    return(
-        <h1>welcome to react project</h1>
-    )
-}
-export default Demo1
--- vite-react-01/src/demo.jsx
+++
@@ -1,6 +0,0 @@
-function Demo(){
-    return(
-        <h2>React is ready</h2>
-    )
-}
-export default Demo
